2fd258238931e57450297018dbd3cdf038adf54c
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2023-11-13 21:40:35 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.DB_DATABASE, //Название БД
+    process.env.DB_NAME, //Название БД
     process.env.DB_USER, //Пользователь
     process.env.DB_PASSWORD, //Пароль
     {
