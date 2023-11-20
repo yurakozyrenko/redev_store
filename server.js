152963e38c034bddc8e554b881b261b28753465f
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2023-11-20 18:29:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const start = async () => {
     try {
         await sequelize.authenticate();
         console.log('Соединение с БД было успешно установлено');
-        await sequelize.sync({ force: true });
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
     } catch (e) {
         console.log('Невозможно выполнить подключение к БД: ', e);
